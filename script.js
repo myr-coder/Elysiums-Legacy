353714bd8761eebf18770bea97af9d2337d12905
Meryl R <denver-meryl@Black-Mamba-2.local>
2023-08-27 21:16:50 +0530

    first

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let text = document.getElementById ('text');
 let sun = document.getElementById ('sun');
-// let wizard = document.getElementById ('wizard');
-let blueFlakes = document.getElementById ('blue-flakes');
 let rightCliff = document.getElementById ('right-cliff');
 let leftFloat = document.getElementById ('left-float');
 let rightFloat = document.getElementById ('right-float');
@@ -15,9 +13,6 @@ let value = window.scrollY;
 
 text.style.marginTop = value * 1.0 + 'px';
 sun.style.left = value * 0.5 + 'px';
-// wizard.style.top = value * -1.5 + 'px';
-blueFlakes.style.top = value * -1.5 + 'px';
-// rightCliff.style.right = value * -0.05 + 'px';
 leftFloat.style.left = value * -0.5 + 'px';
 leftFloat.style.top = value * -0.2 + 'px';
 rightFloat.style.right = value * -0.5 + 'px';
